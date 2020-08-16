551ff56d879b2cd0afaa0cbf82f6a563e220df45
easyadin <tobiayokunnu@gmail.com>
2020-08-16 19:44:03 +0400

    carmart

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { AnimationItem } from 'lottie-web';
+import { AnimationOptions } from 'ngx-lottie';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage implements OnInit, OnDestroy {
+  constructor() { }
+
+  ngOnInit(): void { }
+
+  animationItem: AnimationItem;
+  
+  options: AnimationOptions = {
+    path: '../../assets/main/data.json',
+    autoplay: false,
+  };
+
+
+
+  animationCreated(animationItem: AnimationItem): void {
+    this.animationItem = animationItem;
+  }
+
+  ngOnDestroy() {
+    this.animationItem.destroy();
+  }
+}
